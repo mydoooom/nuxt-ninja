66142f10b3342f7a3bdbe231f106c085d2c720b6
Tran Viet Anh <tran@vietanh.cz>
2022-11-20 02:43:18 +0100

    Initial commit

---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Products</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fugiat id, iste quam recusandae reprehenderit sed tempore! Delectus, eum illum.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
